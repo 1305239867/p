f0f3b90e884af9931bdd1bf54fa304a81b83fac4
zzl <1305239867@qq.com>
2018-11-23 21:48:22 +0800

    ppppp

---
+++ src/com/baizhi/service/impl/AuctionServiceImpl.java
@@ -0,0 +1,54 @@
+package com.baizhi.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baizhi.dao.AuctionDao;
+import com.baizhi.entity.Auction;
+import com.baizhi.service.AuctionService;
+@Service
+public class AuctionServiceImpl implements AuctionService {
+	@Autowired
+	private AuctionDao auctionDao;
+
+	@Override
+	public void update(Auction auction) {
+		// TODO 自动生成的方法存根
+		auctionDao.update(auction);
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO 自动生成的方法存根
+		auctionDao.delete(id);
+	}
+
+	@Override
+	public Auction selectOne(int id) {
+		// TODO 自动生成的方法存根
+		return auctionDao.selectOne(id);
+	}
+
+	@Override
+	public void add(Auction auction) {
+		// TODO 自动生成的方法存根
+		auctionDao.add(auction);
+	}
+
+	@Override
+	public void updatepic(int auction_id, double auction_upset) {
+		// TODO 自动生成的方法存根
+		auctionDao.updatepic(auction_id, auction_upset);
+	}
+
+	@Override
+	public List<Auction> selectAll(String auction_name, String auction_desc, Date auction_start_time,
+			Date auction_end_time, Double auction_start_price) {
+		// TODO 自动生成的方法存根
+		return auctionDao.selectAll(auction_name, auction_desc, auction_start_time, auction_end_time, auction_start_price);
+	}
+
+}
